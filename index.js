3a7fcf0f0f45535c89d2b45631c6231de12adb59
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-06-21 00:47:24 -0400

    Block non-Reddit

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-chrome.browserAction.onClicked.addListener(tab => {
+chrome.browserAction.onClicked.addListener(tab => {   
+    if (!tab.url.includes("reddit.com")) {
+        alert("Reddit is the only compatible site currently.");
+        return;
+    }
+
     chrome.tabs.executeScript(tab.id, {
         file: "anonymize.js",
     });
