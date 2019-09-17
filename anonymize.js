c734996ac80310e18130f5cf0c83177b4b4f66f3
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-09-16 20:25:09 -0400

    Fix ESLint errors

--- anonymize.js
+++ anonymize.js
@@ -1,29 +1,28 @@
 (() => {
-    const colors = [
-        // Primary
-        "red",
-        "blue",
-        "yellow",
-        // Secondary
-        "orange",
-        "lime",
-        "fuchsia",
-        // Brighter primary
-        "pink",
-        "aquamarine",
-        "papayawhip",
-    ]
+	const colors = [
+		// Primary
+		"red",
+		"blue",
+		"yellow",
+		// Secondary
+		"orange",
+		"lime",
+		"fuchsia",
+		// Brighter primary
+		"pink",
+		"aquamarine",
+		"papayawhip",
+	];
 
-    const users = {
-        "haykam821": "goldenrod",
-    };
+	const users = {
+		haykam821: "goldenrod",
+	};
 
-    Array.from(document.querySelectorAll(".author, a[href^='/u/']")).forEach((item, index) => {
-        item.style.color = "transparent";
-        if (!users[item.innerHTML]) {
-            console.log(item.innerHTML, colors[index % colors.length])
-            users[item.innerHTML] = colors[index % colors.length];
-        }
-        item.style.backgroundColor = users[item.innerHTML];
-    });
+	[...document.querySelectorAll(".author, a[href^='/u/']")].forEach((item, index) => {
+		item.style.color = "transparent";
+		if (!users[item.innerHTML]) {
+			users[item.innerHTML] = colors[index % colors.length];
+		}
+		item.style.backgroundColor = users[item.innerHTML];
+	});
 })();
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-chrome.browserAction.onClicked.addListener(tab => {   
-    if (!tab.url.includes("reddit.com")) {
-        alert("Reddit is the only compatible site currently.");
-        return;
-    }
+chrome.browserAction.onClicked.addListener(tab => {
+	if (!tab.url.includes("reddit.com")) {
+		alert("Reddit is the only compatible site currently.");
+		return;
+	}
 
-    chrome.tabs.executeScript(tab.id, {
-        file: "anonymize.js",
-    });
+	chrome.tabs.executeScript(tab.id, {
+		file: "anonymize.js",
+	});
 });
